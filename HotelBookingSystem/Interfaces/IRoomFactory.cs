a503c7b1eb5794381f619fdeaa2a10f3ed86cff3
Raevschi Grigore <pseudo.denver@gmail.com>
2026-02-23 21:33:43 +0200

    refactor(factory-method): remove IRoomFactory and replace with RoomCreator hierarchy

--- HotelBookingSystem/Interfaces/IRoomFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using HotelBookingSystem.Models;
-
-namespace HotelBookingSystem.Interfaces
-{
-     public interface IRoomFactory
-     {
-          Room CreateRoom(string roomId, string roomNumber, decimal basePrice, int capacity);
-     }
-}
