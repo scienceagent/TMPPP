e850381311d9fc4c3ab402d4a0b9772265a2f196
Raevschi Grigore <pseudo.denver@gmail.com>
2026-02-25 22:07:57 +0200

    Add design pattern Singleton

---
+++ HotelBookingSystem/Singleton/HotelAuditLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBookingSystem.Singleton
+{
+     public partial class HotelAuditLogger : Component
+     {
+          public HotelAuditLogger()
+          {
+               InitializeComponent();
+          }
+
+          public HotelAuditLogger(IContainer container)
+          {
+               container.Add(this);
+
+               InitializeComponent();
+          }
+     }
+}
